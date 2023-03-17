3fc667fa51e93121dede065225f76ff06faa9ccc
puri0074 <1472315@senati9.pe>
2023-03-17 10:22:11 -0500

    mi commit2

--- BuscandoElNumeroMayorOMenorQue10EnJava.java
+++ BuscandoElNumeroMayorOMenorQue10EnJava.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+//buscando un numero mayor o menor
+
 package buscando.el.numero.mayor.o.menor.que.pkg10.en.java;
 
-/**
- *
- * @author Senati
- */
+
 public class BuscandoElNumeroMayorOMenorQue10EnJava {
 
     /**
@@ -15,6 +10,20 @@ public class BuscandoElNumeroMayorOMenorQue10EnJava {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+        scanner dato = new Scanner(System.in); //llamar al numero que estamos buscando
+        System.out.println("ingrese el numero; "); //pedimos al personaje que pida un numero 
+        int num1 = dato.nestInt(); //leer el numero
+
+        if (num1<10) {//si el numero es menor que 10 imprime a continuación};
+
     }
-    
+    else if (num1 < 10){ //sin el num 1 es menor que 10 imprime lo siguiente }
 }
+    else{//de lo contrario imprimir lo siguente}
+    System.out.println("hola puri");
+    System.out.println("el numero que ingresaste es menor que 10");
+        }
+    }
+}
+
